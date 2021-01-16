ea9bca734106af87c3898861b2f68de8562a2471
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-01-16 01:40:38 +0000

    Alterations to binding_energy.py code readability + creation of output file.

--- binding_energy.py
+++ binding_energy.py
@@ -2,6 +2,7 @@
 import numpy as np
 
 
+#Define the function for easy use throughout the programme.
 def B(A, Z):	#Binding energy function.
     N = A - Z
     v = a_v * float(A)
@@ -11,15 +12,16 @@ def B(A, Z):	#Binding energy function.
     return v - a - c - a + delta
 
 
+#Asking the user to decide how to proceed for the variable definitions.
 ans = input("Input paramaters? y/n")
 
-if ans == "y":
+if ans == "y":  #Allow user to set all parameters.
     a_v = float(input("Input a_v:"))
     a_s = float(input("Input a_s:"))
     a_c = float(input("Input a_c:"))
     a_a = float(input("Input a_a:"))
     delta = float(input("Input delta:"))
-else:
+else:   #Set parameters automatically.
     print("Using preset parameters")
     a_v = 15.56; a_s = 17.23; a_c = 0.7; a_a = 23.28
     delta = float(input("Input delta:"))
@@ -30,7 +32,11 @@ a_a = {}
 delta = {}""" .format(a_v, a_s, a_c, a_a, delta))
 
 
+#Take the values of each nuclear variable from user.
 A = float(input("Enter A:"))
+#There is a range setting for Z so a range of binding energies can be
+#calculated. (This is useful to me in my studies.) For a single binding energy
+#calculation, enter Zlo=n, Zup=n+1.
 Zlo = float(input("Enter minimum Z:"))
 Zup = float(input("Enter maximum Z:"))
 Z = np.arange(Zlo, Zup, 1)
@@ -38,5 +44,6 @@ Z = np.arange(Zlo, Zup, 1)
 
 file = open("binding_energies.txt", 'w')
 for i in Z:
+    print("Binding energy for Z = {} = {} MeV" .format(i, '%.5f' %B(A, i)))
     file.write(str(B(A, i)) + "\n")
 file.close()
