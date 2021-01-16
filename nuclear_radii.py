73377232008c02a5964d2d2903bb4fc89cca9da3
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-01-16 02:00:53 +0000

    Update nuclear_radii.py
    Added abiity to calculate the ratio of radii of 2 nuclei.

--- nuclear_radii.py
+++ nuclear_radii.py
@@ -7,7 +7,17 @@ def R(A):
     return A**(1/3)
 
 
-#Get the value for A.
-A = float(input("Input the A value of the nuclei:"))
-
-print("The radii =", '%.3f' %R(A), "fm")
+#Adding ability to calculate ratio of atomic radii for past paper exam
+#question.
+ans = input("Are you calculating a radii ratio, y/n?:")
+if ans == "y":  #Calculate a ratio, just as the user says! :)
+    #I like to loop for expandability in the future.
+    A = []
+    for i in range(2):
+        a = float(input("Input an A value:"))
+        A.append(a)
+    print("The ratio of radii of nuclei A = {} to A = {} is: {}"
+    .format(A[0], A[1], '%.3f' %(R(A[0]) / R(A[-1]))))
+else:   #Just calculate a radius.
+    A = float(input("Input an A value:"))
+    print("The radii of nuclei A = {} is: R = {} fm" .format(A, '%.3f' %R(A)))
