b15c75df55a88a11194ae18e04c4bf0ccad87149
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-01-16 17:39:00 +0000

    Updated spin_parity_calculator.py
    Created full list of wood-saxon so energy levels.
    Added dictionary for l number states.
    Wrote fraction print statement for prepresenting j values.

--- Nuclear_Physics/spin_parity_calculator.py
+++ Nuclear_Physics/spin_parity_calculator.py
@@ -1,2 +1,17 @@
 #This programme prints the ground state energy level structure of a nucleus.
 #The structure is based on the Wood-Saxon well with spin-orbit splitting.
+#------------------------------------------------------------------------------
+#Import libraries that help the programme run.
+from numpy import loadtxt   #Used to get data from txt file.
+from fractions import Fraction  #Used for displaying output nicely.
+
+
+#Create dictionary of l numbers.
+l = {0:"s", 1:"p", 2:"d", 3:"f", 4:"g", 5:"h", 6:"i", 7:"j"}
+for i in l:
+    print("{}-level, l = {}" .format(l[i], i))
+
+#
+j = loadtxt("wood_saxon_so_energy_levels.txt")[:, 2]
+for i in j:
+    print(Fraction(int(i*2), 2))
