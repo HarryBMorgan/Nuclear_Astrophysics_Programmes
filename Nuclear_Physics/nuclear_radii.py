846c3bea366915353bf810ec6123193d5bea86d3
Bigharry34 <64849170+Bigharry34@users.noreply.github.com>
2021-01-17 01:08:20 +0000

    Update nuclear_radii.py
    Added some space to make it more readable :)

--- Nuclear_Physics/nuclear_radii.py
+++ Nuclear_Physics/nuclear_radii.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     #question.
     ans = input("Are you calculating a radii ratio, y/n?:")
     if ans == "y":  #Calculate a ratio, just as the user says! :)
+        
         #I like to loop for expandability in the future.
         A = []
         for i in range(2):
@@ -18,7 +19,9 @@ if __name__ == "__main__":
             A.append(a)
         print("The ratio of radii of nuclei A = {} to A = {} is: {}"
         .format(A[0], A[1], '%.3f' %(nuclear_radii(A[0]) / nuclear_radii(A[-1]))))
-    else:   #Just calculate a radius.
+   
+   else:   #Just calculate a radius.
+        
         A = float(input("Input an A value:"))
         print("The radii of nuclei A = {} is: R = {} fm" .format(A, '%.3f' \
             %nuclear_radii(A)))
